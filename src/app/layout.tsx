f6fb7964eb271f389a488944f0565e18da7110e5
yunusmertofficial <yunusmertofficial@gmail.com>
2024-12-01 23:51:51 +0300

    Remove display swap option from local font definitions for GeistSans and GeistMono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,13 +8,11 @@ const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
-  display: "swap", // Font hemen yüklendiğinde sistem fontu ile değişim yapılır
 });
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
-  display: "swap", // Font hemen yüklendiğinde sistem fontu ile değişim yapılır
 });
 
 export const metadata: Metadata = {
