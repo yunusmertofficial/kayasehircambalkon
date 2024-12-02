61e7d3bac564cd96c56eddd638490a83d1a3976b
yunusmertofficial <yunusmertofficial@gmail.com>
2024-12-02 21:21:49 +0300

    Remove local font definitions for GeistSans and GeistMono from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import WhatsAppButton from "@/components/WhatsappButton";
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Kayaşehir Cam Balkon",
@@ -28,9 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`antialiased`}>
         <Header />
         {children}
         <Footer />
