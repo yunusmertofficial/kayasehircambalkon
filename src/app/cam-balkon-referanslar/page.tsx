b81b88a01fd778e890805fb59fde5d7147c8049f
yunusmertofficial <yunusmertofficial@gmail.com>
2024-12-03 21:48:08 +0300

    Refactor Reference component: add index parameter to image mapping and set priority for the first image

--- src/app/cam-balkon-referanslar/page.tsx
+++ src/app/cam-balkon-referanslar/page.tsx
@@ -81,7 +81,7 @@ export default function Reference() {
       )}
       <div className="mx-auto max-w-[1960px] p-4">
         <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
-          {images.map(({ id, public_id, format }) => (
+          {images.map(({ id, public_id, format }, index) => (
             <button
               className="after:content group relative mb-5 h-80 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
               key={id}
@@ -92,6 +92,7 @@ export default function Reference() {
                 alt="Next.js Conf photo"
                 className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110 object-cover w-full h-full"
                 style={{ transform: "translate3d(0, 0, 0)" }}
+                priority={index === 0}
                 placeholder="blur"
                 blurDataURL={`/images/referanslar/blur/${public_id}.${format}`}
                 src={`/images/referanslar/${public_id}.${format}`}
